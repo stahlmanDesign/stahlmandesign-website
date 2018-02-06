6a332502d60b21c794679315fecf11747ec1e710
Justin Stahlman <jstahlman@mac.com>
2018-02-05 22:44:25 -0500

    refactoring Cv

--- stahlman-design-website/src/components/Cv/Header.js
+++ stahlman-design-website/src/components/Cv/Header.js
@@ -1,31 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 import GitHubActivity from './GitHubActivity.js'
+
 class Header extends React.Component {
+  componentWillMount(){
+    this.APP = this.context.ctxStore.APP
+  }
   render() {
+    const APP = this.APP
+    const { dictionary, name, note, title, email, website, phone, about, aboutThisCv, stack, certifications, skills, jobs } = this.props
+    const lang = APP.state.lang
     return <div className="Header col-sm-9">
       <h1 id="name">
-        {this.props.data.name}
+        { name }
       </h1>
       <div
         className="lead adjust-title-style"
         >
         <span dangerouslySetInnerHTML={{
-          __html: this.props.data.title[this.props.lang],
+          __html: title[lang],
           sanitize: true
         }}></span>
       </div>
       <strong>
         <span id="email">
-          <Link to={"mailto:" + this.props.data.email.code}>
-            {this.props.data.email.appearance}
+          <Link to={"mailto:" + email.code}>
+            { email.appearance}
           </Link>&nbsp;&nbsp;</span>
       </strong>
       <strong>
         <span id="phone">
-          <Link to={"tel:" + this.props.data.phone.code}>
+          <Link to={"tel:" + phone.code}>
             <span dangerouslySetInnerHTML={{
-              __html: this.props.data.phone.appearance,
+              __html: phone.appearance,
               sanitize: true
             }}></span>
 
@@ -35,9 +43,9 @@ class Header extends React.Component {
       </strong>
       <strong>
         <span id="website">
-          <Link to={this.props.data.website.code}>
+          <Link to={ website.code}>
             <span dangerouslySetInnerHTML={{
-              __html: this.props.data.website.appearance,
+              __html: website.appearance,
               sanitize: true
             }}></span>
           </Link>
@@ -46,18 +54,21 @@ class Header extends React.Component {
       </strong>
       <p id="note" className="note" >
         <span dangerouslySetInnerHTML={{
-          __html: this.props.data.note[this.props.lang],
+          __html: note[lang],
           sanitize: true
         }}></span>
       </p>
       <h4>
         <a href='https://github.com/stahlmanDesign'>GitHub</a>
-        {this.props.lang === 'en'
+        {lang === 'en'
           ? ' contributions over the last year'
           : ' contributions depuis un an'}
       </h4>
-    <GitHubActivity lang={this.props.lang} />
+    <GitHubActivity lang={lang} />
   </div>
   }
+}
+Header.contextTypes ={
+  ctxStore: PropTypes.object
 }
 export default Header
--- stahlman-design-website/src/components/Cv/index.js
+++ stahlman-design-website/src/components/Cv/index.js
@@ -11,63 +11,32 @@ import SkillsBox from './SkillsBox'
 import cvdata from './cv.json'
 
 class Cv extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: cvdata,
-      lang: 'en'
-    }
-  }
+
   componentWillMount(){
     this.APP = this.context.ctxStore.APP
   }
-  componentDidMount() {
-    // this._loadDataFromServer()  // load ASAP !
-    // setInterval( ()=> {this._loadDataFromServer()},10000) // polling every 10 x seconds
-  }
-  // _loadDataFromServer() {
-  //   $.ajax({
-  //     url: process.env.PUBLIC_URL + '/cv.json',
-  //     dataType: 'json',
-  //     success: (data) => {
-  //       //console.log('success')
-  //       this.setState({data: data})
-  //     },
-  //     error: (xhr, status, err) => {
-  //       console.error(this.props.url, status, err.toString())
-  //     }
-  //   })
-  // }
-  // _setLang(enOrFr) {
-  //   this.setState({lang: enOrFr})
-  // }
-  render() {
-    if (this.state.data === null) {
-      return <div>
-        <img
-          src='load.gif'
-          alt='Waiting for data to load'/>
-      </div>
-    }
-    return <div className='Cv st-pad-top container'>
 
-      <Header
-        data={this.state.data}
-        lang={this.APP.state.lang}/>
+  render() {
+    const APP = this.APP
+    const lang = APP.state.lang
+
+    return <div className='Cv sd-pad-top container'>
+      <div className='pt-5'></div>
+      <Header { ...cvdata }/>
       <div className='row'>
         <div className='col-xs-12 col-sm-7 col-md-5'>
           <CertificationBox
-            certifications={this.state.data.certifications}
-            lang={this.state.lang}/>
+            certifications={ cvdata.certifications }
+            lang={ lang }/>
           <SkillsBox
-            skills={this.state.data.skills}
-            dictionary={this.state.data.dictionary}
-            lang={this.state.lang}/>
+            skills={ cvdata.skills }
+            dictionary={ cvdata.dictionary }
+            lang={ lang }/>
         </div>
       </div>
       <JobBox
-        jobs={this.state.data.jobs}
-        lang={this.state.lang}/>
+        jobs={ cvdata.jobs }
+        lang={ lang }/>
     </div>
   }
 }
